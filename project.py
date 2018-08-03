701681271c3ebc7e5e9ea16d978b4c79ec28da31
Gh0st <otioritsejafor@gmail.com>
2018-08-03 16:11:07 +0100

    Initial commit

---
+++ project.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants')
+@app.route('/restaurants/')
+def restaurantList():
+	return "This page will show all restaurants"
+
+@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurants/<int:restaurant_id>/menu')
+def restaurantMenu():
+	pass
+
+@app.route('/restaurants/new')
+def newRestaurant():
+	pass
+@app.route('/restaurants/<int:restaurant_id>/edit')
+def editRestaurant():
+	pass
+
+@app.route('/restaurants/<int:restaurant_id>/delete')
+def deleteRestaurant():
+	pass
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def editMenuItem():
+	pass
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem():
+	pass
+
+@app.route('/restaurants/<int:restaurant_id>/menu/new')
+def newMenuItem():
+	pass
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
